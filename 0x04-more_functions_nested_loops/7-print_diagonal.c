df8eb601f5778cab552b1a83b1eb21f8e7d94b6a
SeiF_ElDiN <seifmash4@gmail.com>
2023-07-21 17:07:31 -0700

    ylaaa

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,21 @@ void print_diagonal(int n)
 {
 	int postn, space;
 
-	if (n <= 0)
+	if (n < 0)
 		_putchar('\n');
+	if else (n == 0)
+	{
+		_putchar('$');
+		_putchar('\n');
+	}
 	else
 	{
-		for (postn = 1; postn < n; postn++)
+		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = 2; space <= postn; space++)
 				_putchar(' ');
 			_putchar(92); /* is equal to '/' char */
+			_putchar('$');
 			_putchar('\n');
 		}
 	}
